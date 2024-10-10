cc70393d47a34c119bb8932ddbe0c0c1a89a69d2
JOEAI <42526993+jomarbp@users.noreply.github.com>
2024-10-10 14:24:35 -0500

    Create 4. Crud_completo.dart

---
+++ flutter/4. Crud_completo.dart
@@ -0,0 +1,90 @@
+LOGIN FLUTTER
+
+**1. En el proyecto API que tenemos en la ruta C://Xampp/htdocs/api_flutter, deberá ACTUALIZAR el fichero de nombre "api.php"**
+Escribir el comando: 
+
+.. image:: img/Act_api.php1.png
+   :height: 40
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/Act_api.php2.png
+   :height: 40
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/Act_api.php3.png
+   :height: 45
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+**2. En el archivo ListarUsuarios.dart; actualizar el código... que quede así**
+
+.. image:: img/ListarUsuarios1.dart.png
+   :height: 40
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/ListarUsuarios2.dart.png
+   :height: 40
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/ListarUsuarios3.dart.png
+   :height: 45
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/ListarUsuarios4.dart.png
+   :height: 45
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+3. Crear el archivo **EditarUsuario.dart**
+
+.. image:: img/EditarUsuario1.dart.png
+   :height: 40
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/EditarUsuario2.dart.png
+   :height: 40
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/EditarUsuario3.dart.png
+   :height: 45
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/EditarUsuario4.dart.png
+   :height: 40
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+.. image:: img/EditarUsuario5.dart.png
+   :height: 45
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+3. Actualizar el archivo **main.dart**
+
+.. image:: img/Act_MainDart.dart.png
+   :height: 45
+   :width: 90
+   :scale: 10
+   :alt: JoeAI
+
+**Al ejecutar el archivo main.dart, deberá primero iniciar el loginPage.dart, ingresar DATOS REALES DE LA TABLA usuarios.... Al ingresar las credenciales correctas, el app les dará acceso al Dashboard.dart. POSTERIORMENTE podrán dar clic en Listar y verán la tabla con los registros a los cuáles podrán editar o eliminarlo: VER VÍDEO DE CLASE: https://drive.google.com/drive/folders/1Ad6ixdLwRXgP2fkBpAztMFJaw_oeoXKp?usp=sharing**
